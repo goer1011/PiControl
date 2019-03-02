68db6cd184f999d85bc3382e1afb606872f719b4
Erfan Golchini <erfan.golchini@bwedu.de>
2019-03-02 17:51:09 +0100

    V2.0.0

--- PiController.py
+++ PiController.py
@@ -1,57 +1,128 @@
-import sys
-import cgi
+from __future__ import unicode_literals
+
+import hashlib
 import socket
-import RPi.GPIO as GPIO
+import sys
+import protocol
+import cgi
+#import RPi.GPIO as GPIO
 import time
 
-power= "OFF"
+class ProjectorError(Exception):
+    pass
 
-def ProjectorTurn(power, monitor_ip):
-    # if len(sys.argv) == 1:
-    #   form = cgi.FieldStorage()
-    #   monitor_ip = form.getvalue('monitor_ip')
-    # else:
-    #   monitor_ip = sys.argv[1]
-    #   power = sys.argv[2]
-    
-    #define some constanst to make life easier
-    port = 4352
-    buffer_size = 1024
-    
-    if (power == 'ON'):
-        command = '%1POWR 1\r'
-    elif (power == 'OFF'):
-        command = '%1POWR 0\r'
-    else:
-        command = '%1POWR ?\r'
-        print ("Invalid command.")
-        print ("Use: IP ON|OFF")
-        print ("getting current status of device:")
-    
-    new = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    new.connect((monitor_ip, port))
-    recv_data = new.recv(buffer_size)
-    print (recv_data)
-    print ("sending: ", command)
-    new.send(command)
-    recv_data = new.recv(buffer_size)
-    print (recv_data)
-    new.close()
+reverse_dict = lambda d: dict(zip(d.values(), d.keys()))
 
-while True:
+SET_POWER_STATES = {
+    'off': '0',
+    'on': '1',
+    'cooling': '2',
+    'warm-up': '3',
+}
+
+POWER_STATES_REV = reverse_dict(SET_POWER_STATES)
+
+GET_POWER_STATES = '?'
+
+POWR_BODY = 'POWR'
+
+OFF='off'
+
+ON='on'
+
+class Projector(object):
+    def __init__(self, f):
+        self.f = f
+
+    @classmethod
+    def from_address(cls, address, port=4352):
+        """build a Projector from a ip address"""
+        sock = socket.socket()
+        sock.connect((address, port))
+        # in python 3 I need to specify newline, otherwise read hangs
+        # in "PJLINK 0\r"
+        # I expect socket file to return byte strings in python 2 and
+        # unicode strings in python 3
+        if sys.version_info.major == 2:
+            f = sock.makefile()
+        else:
+            f = sock.makefile(mode='rw')
+        return cls(f)
+
+    def authenticate(self, cmd, password=None):
+        # I'm just implementing the authentication scheme designed in the
+        # protocol. Don't take this as any kind of assurance that it's secure.
+
+        data = protocol.read(self.f, 9)
+        assert data[:7] == 'PJLINK '
+        security = data[7]
+        if security == '0':
+            return None
+        data += protocol.read(self.f, 9)
+        assert security == '1'
+        assert data[8] == ' '
+        salt = data[9:17]
+        assert data[17] == '\r'
+
+        if password is None:
+            raise RuntimeError('projector needs a password')
+
+        if callable(password):
+            password = password()
+
+        pass_data = (salt + password).encode('utf-8')
+        pass_data_md5 = hashlib.md5(pass_data).hexdigest()
+
+        # we *must* send a command to complete the procedure,
+        # so we just get the power state.
+
+        cmd_data = protocol.to_binary(POWR_BODY, cmd)
+        self.f.write(pass_data_md5 + cmd_data)
+        self.f.flush()
+
+        # read the response, see if it's a failed auth
+        data = protocol.read(self.f, 7)
+        if data == 'PJLINK ':
+            # should be a failed auth if we get that
+            data += protocol.read(self.f, 5)
+            assert data == 'PJLINK ERRA\r'
+            # it definitely is
+            return False
+
+        # good auth, so we should get a reply to the command we sent
+        body, param = protocol.parse_response(self.f, data)
+
+        # make sure we got a sensible response back
+        assert body == POWR_BODY
+        if param in protocol.ERRORS:
+            raise ProjectorError(protocol.ERRORS[param])
+
+        # but we don't care about the value if we did
+        return param
+
+    # Power
+
+    def get_power(self,password=None):
+        param = self.authenticate(GET_POWER_STATES, password)
+        return POWER_STATES_REV[param]
+
+    def set_power(self, status, password=None, force=False):
+        if not force:
+            assert status in (SET_POWER_STATES)
+        
+        self.authenticate(SET_POWER_STATES[status], password)
+
+
+
+if __name__ == "__main__":
+    projector = Projector.from_address('192.168.0.100')
+
     try:
         GPIO.setwarnings(False) # Ignore warning for now
         GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
         GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
-        if power == 'OFF':
-            GPIO.add_event_detect(10,GPIO.RISING,callback=ProjectorTurn("ON","10.0.0.1")) # Setup event on pin 10 rising edge
-        else:
-            GPIO.add_event_detect(10,GPIO.RISING,callback=ProjectorTurn("OFF","10.0.0.1")) # Setup event on pin 10 rising edge
+        GPIO.add_event_detect(10,GPIO.RISING,callback=projector.set_power(OFF, "ABCDEFG")) # Setup event on pin 10 rising edge
+        GPIO.add_event_detect(10,GPIO.RISING,callback=projector.set_power(ON, "ABCDEFG")) # Setup event on pin 10 rising edge
     except:
         print("Bedienung Fehlgeschlagen")
         time.sleep(30)
-
-
-        
-
-    GPIO.cleanup() # Clean up
